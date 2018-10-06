4ff36cf42987640adfe7ea8cf807fd7073c5bc1e
MaxGorshkov <ma.swiza@gmail.com>
2018-10-06 22:42:59 +0500

    initial commit

---
+++ client.web/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = function(env, argv) {
+  
+  // default to the server configuration
+  const base = {
+    entry: './server/index.ts',
+    output: {
+      filename: 'server.js',
+      // path needs to be an ABSOLUTE file path
+      path: path.resolve(process.cwd(), 'dist'),
+      publicPath: '/',
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: 'cheap-module-eval-source-map',
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+    module: {
+      rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+            }
+          ]
+        },
+        {
+            test: /\.css$/,  
+            loaders: ['ignore-loader'],
+        }
+      ]
+    },
+  }
+  
+  // server-specific configuration
+  if (env.platform === 'server') {
+    base.target = 'node';
+  }
+
+  return base;
+}
